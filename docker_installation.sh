fca0ce275c8c65ca4ff71b61d6cfa103f842a3a5
fatima-latif <fatima.latif@camp1.tkxel.com>
2026-04-03 12:47:40 +0500

    updated docker permission

--- docker_installation.sh
+++ docker_installation.sh
@@ -11,10 +11,10 @@ if command -v docker &> /dev/null; then
 fi
 
 echo "Updating system..."
-apt-get update -y
+sudo apt-get update -y
 
 echo "Installing dependencies..."
-apt-get install -y ca-certificates curl gnupg
+sudo apt-get install -y ca-certificates curl gnupg
 
 # Add Docker GPG key (only if not exists)
 if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
@@ -31,14 +31,14 @@ if [ ! -f /etc/apt/sources.list.d/docker.list ]; then
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
     https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | \
-     tee /etc/apt/sources.list.d/docker.list > /dev/null
+     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
 
 echo "Updating package list..."
-apt-get update -y
+sudo apt-get update -y
 
 echo "Installing Docker..."
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo "Enabling Docker service..."
 systemctl enable docker
